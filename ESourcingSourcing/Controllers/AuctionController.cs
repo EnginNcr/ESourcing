4786805ac5d4ffc71eece17cb5fa12a1c13b17f8
engin nACAR <nacarenginq@gmail.com>
2022-09-10 16:22:01 +0300

    10.09.2022

--- ESourcingSourcing/Controllers/AuctionController.cs
+++ ESourcingSourcing/Controllers/AuctionController.cs
@@ -1,4 +1,11 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using System;
+using System.Collections.Generic;
+using System.Net;
+using System.Threading.Tasks;
+using ESourcingSourcing.Entities;
+using ESourcingSourcing.Reporsitory;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
 
 namespace ESourcingSourcing.Controllers
 {
@@ -6,6 +13,53 @@ namespace ESourcingSourcing.Controllers
     [ApiController]
     public class AuctionController:ControllerBase
     {
-  
+        private readonly IAuctionRepository _auctionRepository;
+        private readonly ILogger<AuctionController> _logger;
+
+        public AuctionController(IAuctionRepository auctionRepository, ILogger<AuctionController> logger)
+        {
+            _auctionRepository = auctionRepository;
+            _logger = logger;
+        }
+        [HttpGet]
+        [ProducesResponseType(typeof(IEnumerable<Auction>),(int)HttpStatusCode.OK)]
+        public async Task<ActionResult<IEnumerable<Auction>>> GetAuctions()
+        {
+           var auctions=  await _auctionRepository.GetAuction();
+            return Ok(auctions);
+        }
+        [HttpGet("{id:length(24)}",Name ="GetAuction")]
+        [ProducesResponseType(typeof(Auction), (int)HttpStatusCode.OK)]
+        [ProducesResponseType((int)HttpStatusCode.NotFound)]
+        public async Task<ActionResult<Auction>> GetAuctions(string id)
+        {
+            var auction = await _auctionRepository.GetAuction(id);
+            if (auction==null)
+            {
+                _logger.LogError($"Auction with id:{id},hasnt been found in database");
+                return NotFound();
+            }
+            return Ok(auction);
+        }
+        [HttpPost]
+        [ProducesResponseType(typeof(Auction), (int)HttpStatusCode.Created)]
+        public async Task<ActionResult<Auction>> CreateAuction([FromBody]Auction auction)
+        {
+            await _auctionRepository.Create(auction);
+            return CreatedAtRoute("GetAuction", new { id = auction.Id }, auction);
+        }
+        [HttpPut]
+        [ProducesResponseType(typeof(Auction), (int)HttpStatusCode.OK)]
+        public async Task<ActionResult<Auction>> UpdateAuction([FromBody] Auction auction)
+        {
+            return Ok(await _auctionRepository.Update(auction));
+        }
+        [HttpDelete("{id:length(24)}")]
+        [ProducesResponseType(typeof(Auction), (int)HttpStatusCode.OK)]
+        public async Task<ActionResult<Auction>> DeleteAuction(string id)
+        {
+            return Ok(await _auctionRepository.Delete(id));
+        }
+
     }
 }
--- ESourcingSourcing/Controllers/BidController.cs
+++ ESourcingSourcing/Controllers/BidController.cs
@@ -1,4 +1,9 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using System.Collections.Generic;
+using System.Net;
+using System.Threading.Tasks;
+using ESourcingSourcing.Entities;
+using ESourcingSourcing.Reporsitory;
+using Microsoft.AspNetCore.Mvc;
 
 namespace ESourcingSourcing.Controllers
 {
@@ -6,5 +11,33 @@ namespace ESourcingSourcing.Controllers
     [ApiController]
     public class BidController:ControllerBase
     {
+        private readonly IBidRepository _bidRepository;
+        public BidController(IBidRepository bidRepository)
+        {
+            _bidRepository = bidRepository; 
+        }
+        [HttpPost]
+        [ProducesResponseType((int)HttpStatusCode.OK)]
+        public async Task<ActionResult> SendBid([FromBody]Bid bid)
+        {
+            await _bidRepository.SendBid(bid);
+            return Ok();
+        }
+        [HttpGet("GetBidByActionId")]
+        [ProducesResponseType(typeof(IEnumerable<Bid>),(int)HttpStatusCode.OK)]
+        public async Task<ActionResult<IEnumerable<Bid>>> GetBidByAuctionId(string id)
+        {
+            IEnumerable<Bid> bids = await _bidRepository.GetBidsByAuctionId(id);
+            return Ok(bids);
+
+        }
+        [HttpGet("GetWinnerBid")]
+        [ProducesResponseType(typeof(Bid), (int)HttpStatusCode.OK)]
+        public async Task<ActionResult<Bid>> GetWinnerBid(string id)
+        {
+            Bid bid = await _bidRepository.GetBidById(id);
+            return Ok();
+
+        }
     }
 }
